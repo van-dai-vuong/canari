66a90b2ba1904245b83dea1562b12fb9a628eb55
James-A <james.a.goulet@gmail.com>
2024-10-01 14:01:06 -0400

    Correct spacing and typos.

--- examples/time_series_toy.py
+++ examples/time_series_toy.py
@@ -24,28 +24,28 @@ from pytagi.nn import LSTM, Linear, Sequential
 
 from baseline_component import Local_level, Local_trend, Local_acceleration
 from periodic_component import Periodic
-from residual_component import Autoregression_deterministic
+from residual_component import Autoregression
 
 # initialize with no param, no initial hiddens states
-LL = Local_level ()
+LL = Local_level()
 LT = Local_trend()
 LA = Local_acceleration()
-AR = Autoregression_deterministic()
-P  = Periodic(period=52)
+AR = Autoregression()
+P  = Periodic(period = 52)
 
 # initialize with param
-LL_param = Local_level (sigma_w=0.1)
-LT_param = Local_trend(sigma_w=0.1)
-LA_param = Local_acceleration(sigma_w=0.1)
-AR_param = Autoregression_deterministic(sigma_AR=0.1, phi_AR=0.9)
-P_param  = Periodic(sigma_w=0.1, period=52)
+LL_param = Local_level(sigma = 0.1)
+LT_param = Local_trend(sigma = 0.1)
+LA_param = Local_acceleration(sigma = 0.1)
+AR_param = Autoregression(sigma = 0.1, phi = 0.9)
+P_param  = Periodic(sigma = 0.1, period = 52)
 
 # initialize with param and initial hiddens states
-LL_param_hs = Local_level (sigma_w=0.1, mu_x = [0.1], var_x=[0.1])
-LT_param_hs = Local_trend(sigma_w=0.1, mu_x = [0.1, 0.2], var_x=[0.1, 0.2])
-LA_param_hs = Local_acceleration(sigma_w=0.1, mu_x = [0.1, 0.2, 0.3], var_x=[0.1, 0.2, 0.3])
-AR_param_hs = Autoregression_deterministic(sigma_AR=0.1, phi_AR=0.9, mu_x = [0.1], var_x=[0.1])
-P_param_hs  = Periodic(sigma_w=0.1, period=52, mu_x = [0.1, 0.2], var_x=[0.1, 0.2])
+LL_param_hs = Local_level(sigma = 0.1, mu = [0.1], var = [0.1])
+LT_param_hs = Local_trend(sigma = 0.1, mu = [0.1, 0.2], var = [0.1, 0.2])
+LA_param_hs = Local_acceleration(sigma =0.1, mu = [0.1, 0.2, 0.3], var = [0.1, 0.2, 0.3])
+AR_param_hs = Autoregression(sigma = 0.1, phi = 0.9, mu = [0.1], var = [0.1])
+P_param_hs  = Periodic(sigma = 0.1, period = 52, mu = [0.1, 0.2], var =[0.1, 0.2])
 
 check = 1
 
