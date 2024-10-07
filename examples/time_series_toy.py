e1c5f09a6579f5d9398b3899163912a129647f99
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2024-10-07 16:46:11 -0400

    Update time_series_toy.py

--- examples/time_series_toy.py
+++ examples/time_series_toy.py
@@ -2,18 +2,35 @@ from baseline_component import LocalLevel, LocalTrend, LocalAcceleration
 from periodic_component import Periodic
 from residual_component import Autoregression
 from lstm_component import Lstm
-from lstm_ssm_model import LstmSsm
 
-# # initialize with no param, no initial hiddens states
+# initialize with no param, no initial hiddens states
 local_level = LocalLevel()
 local_trend = LocalTrend()
 local_acceleration = LocalAcceleration()
-periodic = Periodic(period=1)
-autoregression = Autoregression(phi=1)
-lstm = Lstm(look_back_len=52, num_features=1, num_layer=2, num_hidden_unit=50)
+autoregression = Autoregression()
 
-model = LstmSsm(local_level, periodic, autoregression)
+# initialize with param, no initial hiddens states
+local_level_1 = LocalLevel(std_error=0.1)
+local_trend_1 = LocalTrend(std_error=0.1)
+local_acceleration_1 = LocalAcceleration(std_error=0.1)
+periodic_1 = Periodic(period=52)
+autoregression_1 = Autoregression(std_error=0.1)
+
+# initialize with param, initial hiddens states
+local_level_2 = LocalLevel(std_error=0.1, mu_states=[0.1], var_states=[0.1])
+local_trend_2 = LocalTrend(std_error=0.1, mu_states=[0.1, 0.1], var_states=[0.1, 0.1])
+local_acceleration_2 = LocalAcceleration(
+    std_error=0.1, mu_states=[0.1, 0.1, 0.1], var_states=[0.1, 0.1, 0.1]
+)
+periodic_2 = Periodic(period=52, mu_states=[0.1, 0.1], var_states=[0.1, 0.1])
+autoregression_2 = Autoregression(std_error=0.1, mu_states=[0.1], var_states=[0.1])
+
+#  lstm
+lstm = Lstm(
+    look_back_len=10,
+    num_features=1,
+    num_layer=2,
+    num_hidden_unit=50,
+)
 
 check = 1
-
-# [[np.cos(w), np.sin(w)], [-np.sin(w), np.cos(w)]]
