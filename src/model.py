90e94a511577cfb938cd282f07e81f6d41cd715a
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2024-10-11 15:16:42 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,11 +2,11 @@ import numpy as np
 from typing import Optional, List
 from pytagi.nn import Sequential
 from base_component import BaseComponent
-
+from base_model import BaseModel
 from common import block_diag
 
 
-class Model:
+class Model(BaseModel):
     """
     LSTM/SSM model
     """
