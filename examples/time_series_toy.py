5a81a4c119ea0894b725517cd3cae6e7269a737c
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2024-10-30 16:07:44 -0400

    remove one dataset

--- examples/time_series_toy.py
+++ examples/time_series_toy.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src import (
     LocalTrend,
+    LocalAcceleration,
     LstmNetwork,
     Periodic,
     Autoregression,
     WhiteNoise,
     Model,
-    PlotStates,
+    PlotWithUncertainty,
 )
 from examples import DataProcess
 import time
@@ -17,6 +18,8 @@ import time
 # # Read data
 data_file = "./data/toy_time_series/sine.csv"
 df_raw = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
+linear_space = np.linspace(0, 2, num=len(df_raw))
+df_raw = df_raw.add(linear_space, axis=0)
 
 data_file_time = "./data/toy_time_series/sine_datetime.csv"
 time_series = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
@@ -30,14 +33,14 @@ df = df_raw.resample("H").mean()
 
 # Define parameters
 output_col = [0]
-num_epoch = 20
+num_epoch = 50
 
 data_processor = DataProcess(
     data=df,
     time_covariates=["hour_of_day", "day_of_week"],
     train_start="2000-01-01 00:00:00",
-    train_end="2000-01-08 21:00:00",
-    validation_start="2000-01-08 22:00:00",
+    train_end="2000-01-07 21:00:00",
+    validation_start="2000-01-07 22:00:00",
     validation_end="2000-01-09 23:00:00",
     output_col=output_col,
 )
@@ -47,14 +50,16 @@ train_data, validation_data, test_data = data_processor.get_splits()
 model = Model(
     LocalTrend(),
     LstmNetwork(
-        look_back_len=24,
+        look_back_len=10,
         num_features=3,
         num_layer=2,
         num_hidden_unit=50,
+        device="cpu",
     ),
     Autoregression(),
-    WhiteNoise(std_error=0.1),
+    WhiteNoise(std_error=1e-3),
 )
+model.auto_initialize_baseline_states(train_data["y"])
 
 # Training
 for epoch in range(num_epoch):
@@ -62,33 +67,24 @@ for epoch in range(num_epoch):
         train_data=train_data, validation_data=validation_data
     )
 
-    mu_smooths = smoother_states.mu_smooths
-    var_smooths = smoother_states.var_smooths
-
-    # #  Plot
-    plt.figure(figsize=(10, 6))
-    plt.plot(data_processor.train_time, train_data["y"], color="r", label="train_obs")
-    plt.plot(
-        data_processor.validation_time,
-        validation_data["y"],
-        color="r",
-        linestyle="--",
-        label="validation_obs",
-    )
-    PlotStates(
-        time=data_processor.train_time,
-        mu=mu_smooths,
-        var=var_smooths,
-        index=0,
-        color="k",
-    )
-    PlotStates(
-        time=data_processor.validation_time,
-        mu=mu_validation_preds,
-        var=var_validation_preds,
-        color="b",
-    )
-    plt.legend()
-    filename = f"saved_results/smoother#{epoch}.png"
-    plt.savefig(filename)
-    plt.close()
+# #  Plot
+plt.figure(figsize=(10, 6))
+plt.plot(data_processor.train_time, train_data["y"], color="r", label="train_obs")
+plt.plot(
+    data_processor.validation_time,
+    validation_data["y"],
+    color="r",
+    linestyle="--",
+    label="validation_obs",
+)
+PlotWithUncertainty(
+    time=data_processor.validation_time,
+    mu=mu_validation_preds,
+    var=var_validation_preds,
+    color="b",
+    label=["mu_val_pred", "±1σ"],
+)
+plt.legend()
+filename = f"saved_results/time_series_toy.png"
+plt.savefig(filename)
+plt.close()
