4453f0b74d454c13c6ec8b765fa327316f02e3b9
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-05-01 09:09:37 -0400

    SKF to skf.py

--- examples/toy_forecast_ar_lstm.py
+++ examples/toy_forecast_ar_lstm.py
@@ -5,16 +5,14 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import pytagi.metric as metric
 from pytagi import Normalizer as normalizer
-from canari.data_process import DataProcess
-from canari.component.baseline_component import LocalTrend
-from canari.component.lstm_component import LstmNetwork
-from canari.component.autoregression_component import Autoregression
-from canari.model import Model
-from canari.data_visualization import (
+from canari import (
+    DataProcess,
+    Model,
     plot_data,
     plot_prediction,
     plot_states,
 )
+from canari.component import LocalTrend, LstmNetwork, Autoregression
 
 
 ###########################
