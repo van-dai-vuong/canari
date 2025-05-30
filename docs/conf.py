30ba419596098968d1f18987be5aed26f56b3f1d
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-05-30 16:04:15 -0400

    docs workflow

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,9 @@
 import os
 import sys
 
-HERE = os.path.dirname(__file__)
-ROOT = os.path.abspath(os.path.join(HERE, os.pardir))
-SRC = os.path.join(ROOT, "src")
-sys.path.insert(0, SRC)
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+)
 
 project = "canari"
 copyright = "2025, Van-Dai Vuong, Luong-Ha Nguyen, James-A. Goulet"
@@ -12,15 +11,17 @@ author = "Van-Dai Vuong, Luong-Ha Nguyen, James-A. Goulet"
 release = "v.0.0.2"
 
 extensions = [
-    "sphinx.ext.autodoc",  # Auto-import docstrings
-    "sphinx.ext.napoleon",  # Google/NumPy style
-    "sphinx.ext.viewcode",  # Add source links
-    "sphinx.ext.autosummary",  # Summary tables (optional)
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
     "nbsphinx",
     "sphinx.ext.mathjax",
     "sphinx_book_theme",
 ]
 nbsphinx_execute = "never"
+
+autodoc_mock_imports = ["pytagi"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
